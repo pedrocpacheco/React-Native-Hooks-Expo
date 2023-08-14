30fa079130b0190a864388dabdfdb25e317733c0
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-14 18:31:13 -0300

    feat: useState to tytle and list of Cyclists

--- src/views/Home/components/Cyclists.tsx
+++ src/views/Home/components/Cyclists.tsx
@@ -1,12 +1,28 @@
-import React, {useEffect} from "react"
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react"
+import { FlatList, Text, StyleSheet } from "react-native";
 
 import { getCyclists } from "../../../services/getData";
 
 export default function Cyclists(){
+  const [tytle, setTytle] = useState(""); // ? Inicial
+  const [list, setList] = useState([]);
+
   useEffect(() => {
     const retorno = getCyclists();
+    setTytle(retorno.tytle); // ! Seta aqui
+    setList(retorno.list)
+    console.log(retorno);
+    
   }, []);
 
-  return <Text>Ciclistas</Text>
+  const ListHeader = () => {
+    return <Text>{ tytle } </Text>
+  }
+
+  return <FlatList
+          data={list}
+          renderItem={({ item: { name } }) => <Text>{name}</Text>}
+          ListHeaderComponent={ ListHeader } 
+          />
 }
+
